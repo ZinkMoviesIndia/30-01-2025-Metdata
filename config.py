e108ea5f6560afd18f21082a6ae01fc8d47f4b2a
ZinkMoviesIndia <162055078+ZinkMoviesIndia@users.noreply.github.com>
2025-01-31 09:57:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
 # REQUIRED CONFIG
 BOT_TOKEN = "7566651613:AAGrJ6dffiyN8Xz_fL2As-WUDDkCDo3vZEw"
-OWNER_ID = "6004363830"
-TELEGRAM_API = "25101360"
+OWNER_ID = 6004363830
+TELEGRAM_API = 25101360
 TELEGRAM_HASH = "136cc2036074e0c1db3c2f3070e3cd7d"
 
 # SEMI-REQUIRED, WE SUGGEST TO FILL IT FROM MONGODB
-DATABASE_URL = "mongodb+srv://ZinkMovies:0FmbEsW3aH0Me7no@zinkmovies.wn3rs.mongodb.net/?retryWrites=true&w=majority&appName=ZinkMovies
-TELEGRAM_API=25101360
-TELEGRAM_HASH=136cc2036074e0c1db3c2f3070e3cd7d"
+DATABASE_URL = "mongodb+srv://ZinkMovies:0FmbEsW3aH0Me7no@zinkmovies.wn3rs.mongodb.net/?retryWrites=true&w=majority&appName=ZinkMovies"
 
 # OPTIONAL CONFIG
 USER_SESSION_STRING = ""
